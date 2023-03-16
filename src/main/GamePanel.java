5c273ffb2085bdfb210ae070986140f9e26b6ea3
DucDuyITITIU21008 <127607170+DucDuyITITIU21008@users.noreply.github.com>
2023-03-16 13:07:08 +0700

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,13 +16,19 @@ public class GamePanel extends JPanel implements Runnable {
 	final int screenWidth = tileSize * maxScreenCol;
 	final int screenHeight = tileSize * maxScreenRow;
 	
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	
+	//Set player's default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	//This is GamePanel
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 public void startgameThread() {
 	gameThread = new Thread(this);
@@ -37,7 +43,7 @@ public void startgameThread() {
 	}
 			
 		public void update (){
-		
+			
 		}
 		public void paintComponent(Graphic g){
 			super.paintComponent(g);
@@ -46,7 +52,7 @@ public void startgameThread() {
 			
 			g2.setcolor(Color.white);
 			
-			g2.fillRect(100,100,tileSize,tileSize);
+			g2.fillRect(playerX,playerY,tileSize,tileSize);
 			
 			g2.dispose();
 		}
